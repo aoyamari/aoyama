915d11d02d4c14092fa1f3b5868f166f24341723
Aoyama <91828307+aoyamari@users.noreply.github.com>
2024-03-19 23:28:50 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-dadf2e82-1020-491b-bca7-d14594f05487/)
 
 
 echo "$curl_output" >> Update.tsx
